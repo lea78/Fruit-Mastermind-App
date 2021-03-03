69ec4acf34f18137c5e7fa66ae4c58400d7d4d23
Nicolas Maurice <nicolas.maurice@edu.itescia.fr>
2021-03-03 22:15:51 +0100

    Update MainActivity.java
    Created fruits and its array

--- app/src/main/java/com/example/fruitmastermind/MainActivity.java
+++ app/src/main/java/com/example/fruitmastermind/MainActivity.java
@@ -6,6 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fruit fraise = new Fruit(false,false, "Fraise");
+    Fruit banane = new Fruit(false,true, "Banana");
+    Fruit framboise = new Fruit(false, false, "Framboise");
+    Fruit kiwi = new Fruit(false, true, "Kiwi");
+    Fruit orange = new Fruit(true, true, "Orange");
+    Fruit prune = new Fruit(true, false, "Prune");
+    Fruit raisin = new Fruit(true, false, "Raisin");
+    Fruit citron = new Fruit(true, true, "Ctiron");
+
+    Fruit fruitArray[] = {fraise,banane,framboise,kiwi,orange,prune,raisin,citron};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
