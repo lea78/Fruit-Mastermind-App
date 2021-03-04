1d5826b2d6733023b1f8e2e25588ec3d53b90387
lea78 <lea.hamzi@edu.itescia.fr>
2021-03-04 11:12:37 +0100

    Creation of header menu with clues

--- app/src/main/java/com/example/fruitmastermind/MainActivity.java
+++ app/src/main/java/com/example/fruitmastermind/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.fruitmastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar toolbar = findViewById(R.id.toolBar);
+        //setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.header, menu);
+        ;
+        return true;
+    }
+
+
+
 }
