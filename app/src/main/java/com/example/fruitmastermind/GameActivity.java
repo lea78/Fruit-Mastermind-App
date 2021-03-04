e9e7083900f7855fe46c38052debba54ca5b02d4
DjouliCorn <tissier.julie@gmail.com>
2021-03-04 15:16:54 +0100

    le grand rassemblement

--- app/src/main/java/com/example/fruitmastermind/GameActivity.java
+++ app/src/main/java/com/example/fruitmastermind/GameActivity.java
@@ -3,12 +3,34 @@ package com.example.fruitmastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.example.fruitmastermind.GameClasses.Fruit;
+import com.example.fruitmastermind.GameClasses.FruitArray;
 
 public class GameActivity extends AppCompatActivity {
+
+    FruitArray gBoard = new FruitArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Fruit[] myCombo = gBoard.generateFruitBoard();
+        for (int i = 0; i < myCombo.length; i++){
+            Log.v("fruit name",myCombo[i].getName());
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.header, menu);
+
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/fruitmastermind/MainActivity.java
+++ app/src/main/java/com/example/fruitmastermind/MainActivity.java
@@ -1,37 +1,18 @@
 package com.example.fruitmastermind;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.util.Log;
-
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
-import android.widget.Toolbar;
-
-
-import com.example.fruitmastermind.GameClasses.Fruit;
-import com.example.fruitmastermind.GameClasses.FruitArray;
 
 public class MainActivity extends AppCompatActivity {
-
-    FruitArray gBoard = new FruitArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Fruit[] myCombo = gBoard.generateFruitBoard();
-        for (int i = 0; i < myCombo.length; i++){
-            Log.v("fruit name",myCombo[i].getName());
-        }
     }
-
 
     // Start GameActivity
 
@@ -49,13 +30,5 @@ public class MainActivity extends AppCompatActivity {
         intent.addCategory(Intent.CATEGORY_HOME);
         startActivity(intent);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.header, menu);
-
-        return true;
-    }
-
 
 }
